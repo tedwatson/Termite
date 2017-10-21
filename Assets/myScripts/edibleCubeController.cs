ecf81973d63d943f7f9b9327950b6aac94a8084f
tedwatson123 <tedwatson123@mac.com>
2017-10-21 01:08:42 -0400

    Created basic prototype of scaling

---
+++ Assets/myScripts/edibleCubeController.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class edibleCubeController : MonoBehaviour {
+
+    public float inc;
+
+    private GameObject cameraRig;
+
+    // Use this for initialization
+    void Start () {
+        cameraRig = GameObject.Find("[CameraRig]");
+        
+
+    }
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    private void OnTriggerEnter(Collider other)
+    {
+        print("!");
+        Vector3 scale = cameraRig.transform.localScale;
+        cameraRig.transform.localScale += new Vector3(scale.x + inc, scale.y + inc, scale.z + inc);
+        Destroy(this.gameObject);
+        
+        
+        
+        
+    }
+}
